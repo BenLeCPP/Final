333efa6ddbe2f563e3147b7bfc8de20d88544d75
Ben Le <hale@cpp.edu>
2021-12-01 15:22:32 -0800

    step 47

--- kilo.c
+++ kilo.c
@@ -14,6 +14,13 @@
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+enum editorKey {
+  ARROW_LEFT = 'a',
+  ARROW_RIGHT = 'd',
+  ARROW_UP = 'w',
+  ARROW_DOWN = 's'
+};
+
 /*** data ***/
 
 struct editorConfig {
