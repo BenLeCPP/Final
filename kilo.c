424379b0b478b4411c0fd5b1b0aba36d26482698
Ben Le <hale@cpp.edu>
2021-12-01 14:47:37 -0800

    step 22

--- kilo.c
+++ kilo.c
@@ -47,6 +47,12 @@ char editorReadKey() {
   return c;
 }
 
+/*** output ***/
+
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+}
+
 /*** input ***/
 
 void editorProcessKeypress() {
@@ -63,6 +69,7 @@ void editorProcessKeypress() {
 int main() {
   enableRawMode();
   while (1) {
+    editorRefreshScreen();
     editorProcessKeypress();
   }
   return 0;
