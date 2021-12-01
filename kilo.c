43f353bfc337832da9adad55bd861da80d348077
Ben Le <hale@cpp.edu>
2021-12-01 15:08:02 -0800

    step 29

--- kilo.c
+++ kilo.c
@@ -70,10 +70,11 @@ int getWindowSize(int *rows, int *cols) {
 
 void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
+
 void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4);
   write(STDOUT_FILENO, "\x1b[H", 3);
