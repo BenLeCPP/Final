69030a250885eafde851e79fff2c66eeed7a394d
Ben Le <hale@cpp.edu>
2021-12-02 20:13:39 -0800

    step 90

--- kilo.c
+++ kilo.c
@@ -364,6 +364,12 @@ void editorProcessKeypress() {
     case PAGE_UP:
     case PAGE_DOWN:
       {
+        if (c == PAGE_UP) {
+          E.cy = E.rowoff;
+        } else if (c == PAGE_DOWN) {
+          E.cy = E.rowoff + E.screenrows - 1;
+          if (E.cy > E.numrows) E.cy = E.numrows;
+        }
         int times = E.screenrows;
         while (times--)
           editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
@@ -377,7 +383,6 @@ void editorProcessKeypress() {
       break;
   }
 }
-
 /*** init ***/
 
 void initEditor() {
