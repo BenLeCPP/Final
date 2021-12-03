25b27edcc85110c5d452936d7fefec1139469270
Ben Le <hale@cpp.edu>
2021-12-02 20:22:25 -0800

    step 99

--- kilo.c
+++ kilo.c
@@ -318,6 +318,7 @@ void editorDrawStatusBar(struct abuf *ab) {
     }
   }
   abAppend(ab, "\x1b[m", 3);
+  abAppend(ab, "\r\n", 2);
 }
 
 void editorRefreshScreen() {
@@ -436,7 +437,7 @@ void initEditor() {
   E.statusmsg[0] = '\0';
   E.statusmsg_time = 0;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
-  E.screenrows -= 1;
+  E.screenrows -= 2;
 }
 
 int main(int argc, char *argv[]) {
