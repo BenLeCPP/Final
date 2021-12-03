a6e4039652473a713573ab819aa66ed1c13a28a8
Ben Le <hale@cpp.edu>
2021-12-02 20:04:47 -0800

    step 76

--- kilo.c
+++ kilo.c
@@ -267,6 +267,7 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
@@ -274,7 +275,9 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_RIGHT:
+      if (row && E.cx < row->size) {
         E.cx++;
+      }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
