2af9ff366840fa28bde0dbe028145942cb183171
Ben Le <hale@cpp.edu>
2021-12-02 20:27:27 -0800

    step 104

--- kilo.c
+++ kilo.c
@@ -24,6 +24,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+  BACKSPACE = 127,
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
@@ -419,6 +420,9 @@ void editorMoveCursor(int key) {
 void editorProcessKeypress() {
   int c = editorReadKey();
   switch (c) {
+    case '\r':
+      /* TODO */
+      break;
     case CTRL_KEY('q'):
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
@@ -430,6 +434,11 @@ void editorProcessKeypress() {
     case END_KEY:
       if (E.cy < E.numrows)
         E.cx = E.row[E.cy].size;
+      break;
+    case BACKSPACE:
+    case CTRL_KEY('h'):
+    case DEL_KEY:
+      /* TODO */
       break;
     case PAGE_UP:
     case PAGE_DOWN:
@@ -451,7 +460,9 @@ void editorProcessKeypress() {
     case ARROW_RIGHT:
       editorMoveCursor(c);
       break;
-      
+    case CTRL_KEY('l'):
+    case '\x1b':
+      break;
     default:
       editorInsertChar(c);
       break;
