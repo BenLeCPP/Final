44ed3b19a03d849d623cb2d01ef420a293a3d196
Ben Le <hale@cpp.edu>
2021-12-02 19:57:04 -0800

    step 65

--- kilo.c
+++ kilo.c
@@ -167,17 +167,11 @@ void editorOpen(char *filename) {
   char *line = NULL;
   size_t linecap = 0;
   ssize_t linelen;
-  linelen = getline(&line, &linecap, fp);
-  if (linelen != -1) {
+  while ((linelen = getline(&line, &linecap, fp)) != -1) {
     while (linelen > 0 && (line[linelen - 1] == '\n' ||
                            line[linelen - 1] == '\r'))
       linelen--;
     editorAppendRow(line, linelen);
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
   }
   free(line);
   fclose(fp);
